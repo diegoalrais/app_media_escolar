6020421dc4bd7a90391d3ba3793d5a4a0f1583d0
Diego Morais <diegoalrais@gmail.com>
2022-03-16 15:46:15 -0300

    second commit - Altera a formatação da media para decimal com apenas uma casa após o ponto

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ Resolvi sair da zona de conforto. Vou enumerar minhas dificuldades:
 1. Projetar como seria o projeto;
 2.  Desinstalei o Android Studio duas vezes na tentativa de voltar a funcionar o emulador. Infelizmente não consegui;
 3. Usando meu celular físico não conseguir fazer debugs para testar, pois várias vezes desconectava do computador (não entendi);
-4. Tive muita dificuldade em fazer o resultado aparecer conforme minha lógica. Pausei por dias o código até voltar a mexer nele.
+4. Tive muita dificuldade em fazer o resultado aparecer conforme minha lógica. Pausei por dias o código até voltar a mexer nele;
+5. Alterei a forma de transformação das strings de int para double;
+6. Coloquei outra val para que formatasse o número decimal em sua exibição (no caso a média).
+
 
 Como forma de desenvolvimento cito:
 
--- app/src/main/java/com/example/media_escolar/MainActivity.kt
+++ app/src/main/java/com/example/media_escolar/MainActivity.kt
@@ -2,14 +2,16 @@ package com.example.media_escolar
 
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
 import com.example.media_escolar.databinding.ActivityMainBinding
+import java.lang.Double.parseDouble
 import java.lang.Integer.parseInt
+import java.text.DecimalFormat
 
-//import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,19 +31,20 @@ class MainActivity : AppCompatActivity() {
 
        binding.btCalulcarMedia.setOnClickListener {
            val nome = nomeAluno.text.toString()
-           val nota1: Int = parseInt(trabalho1.text.toString())
-           val nota2: Int = parseInt(trabalho2.text.toString())
-           val nota3: Int = parseInt(teste.text.toString())
-           val nota4: Int = parseInt(prova.text.toString())
+           val nota1: Double = parseDouble(trabalho1.text.toString())
+           val nota2: Double = parseDouble(trabalho2.text.toString())
+           val nota3: Double = parseDouble(teste.text.toString())
+           val nota4: Double = parseDouble(prova.text.toString())
+           val decimal = DecimalFormat("##.#")
 
            val media = (nota1 + nota2 + nota3 + nota4) / 4
-           //val dec = DecimalFormat("##.#") ${dec.format(media)} ${dec.format(media)}
+
 
            if (media >= 5) {
-               resultado.text = "Média final: $media. \nAluno(a) $nome está APROVADO!"
+               resultado.text = "Média final: ${decimal.format(media)} \nAluno(a) $nome está APROVADO!"
                //resultado.setTextColor(getColor(R.color.blue))
            } else {
-               resultado.text= "Média final: $media. \nAluno(a) $nome está REPROVADO."
+               resultado.text= "Média final: ${decimal.format(media)} \nAluno(a) $nome está REPROVADO."
                //resultado.setTextColor(getColor(R.color.red))
            }
 
@@ -56,31 +59,6 @@ class MainActivity : AppCompatActivity() {
            }
        }
 
-
-
-
-    /*
-
-
-        val valor1 = Double.parseDouble(nota1.text.toString())
-        val valor2 = Double.parseDouble(nota2.text.toString())
-        val valor3 = Double.parseDouble(nota3.text.toString())
-        val valor4 = Double.parseDouble(nota4.text.toString())
-
-
-        binding.btCalulcarMedia.setOnClickListener {
-
-            val media = (valor1 + valor2 + valor3 + valor4) /4
-            val dec = DecimalFormat("##.#")
-
-            if (media >= 6) {
-                resultado.setText("O alunos(a) $nomeAluno foi aprovado com sucesso. \n Sua média final foi: ${dec.format(media)} ")
-                resultado.setTextColor(getColor(R.color.blue))
-            } else {
-                resultado.setText("O alunos(a) $nomeAluno foi reprovado com sucesso. \n Sua média final foi: ${dec.format(media)} ")
-                resultado.setTextColor(getColor(R.color.red))
-            }
-        }*/
-    }
+     }
 }
 
